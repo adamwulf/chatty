9af358a9ad361c4359cce78afa67f5ae0ad9a8e6
Adam Wulf <adam.wulf@gmail.com>
2015-12-13 19:28:54 -0600

    added target info files

---
+++ chattyios/chattyios.h
@@ -0,0 +1,19 @@
+//
+//  chattyios.h
+//  chattyios
+//
+//  Created by Adam Wulf on 12/13/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for chattyios.
+FOUNDATION_EXPORT double chattyiosVersionNumber;
+
+//! Project version string for chattyios.
+FOUNDATION_EXPORT const unsigned char chattyiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chattyios/PublicHeader.h>
+
+
---
+++ chattytv/chattytv.h
@@ -0,0 +1,19 @@
+//
+//  chattytv.h
+//  chattytv
+//
+//  Created by Adam Wulf on 12/13/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for chattytv.
+FOUNDATION_EXPORT double chattytvVersionNumber;
+
+//! Project version string for chattytv.
+FOUNDATION_EXPORT const unsigned char chattytvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chattytv/PublicHeader.h>
+
+
